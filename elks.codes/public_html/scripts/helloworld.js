59da27cb1ad7da010c45596cea50df558bc00542
Soumya Kalluri <soumya.kalluri11@gmail.com>
2021-04-13 19:25:14 +0000

    commit before merge to test deployment

---
+++ elks.codes/public_html/scripts/helloworld.js
@@ -0,0 +1,57 @@
+window.addEventListener("DOMContentLoaded", function() {
+    console.log("Hello World!");
+});
+
+var ex1 = "123"; 
+var ex2 = "456"; 
+var ex3 = "789";
+
+
+console.log("Hello there, people looking at the console of the browser's developer tools");
+
+var ex10 = "123"; 
+var ex21 = "456"; 
+var ex31 = "789";
+var ex11 = "123"; 
+var ex21 = "456"; 
+var ex31 = "789";
+
+
+
+var ex11 = "123"; 
+var ex22 = "456"; 
+var ex32 = "789";
+var ex12 = "123"; 
+var ex22 = "456"; 
+var ex32 = "789";
+
+
+var ex12 = "123"; 
+var ex23 = "456"; 
+var ex33 = "789";
+var ex13 = "123"; 
+var ex23 = "456"; 
+var ex33 = "789";
+
+var ex14 = "123"; 
+var ex24 = "456"; 
+var ex35 = "789";
+var ex16 = "123"; 
+var ex25 = "456"; 
+var ex37 = "789";
+
+
+var lex14 = "123"; 
+var lex24 = "456"; 
+var lex35 = "789";
+var lex16 = "123"; 
+var lex25 = "456"; 
+var lex37 = "789";
+
+var lex1 = "123"; 
+var lex2 = "456"; 
+var lex3 = "789";
+var lex1 = "123"; 
+var lex2 = "456"; 
+var lex3 = "789";
+
