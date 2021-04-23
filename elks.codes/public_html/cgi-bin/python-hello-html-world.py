1c3c7bca9badc8aff3a2b7360b916302f9e791e7
Amrit-S <aksingh@ucsd.edu>
2021-04-22 21:12:15 -0700

    test

--- elks.codes/public_html/cgi-bin/python-hello-html-world.py
+++ elks.codes/public_html/cgi-bin/python-hello-html-world.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import datetime
 import os
-  
-  #Print HTML header
+
+print("Content-Type: text/html")
+#Print HTML header
 print("Cache-Control: no-cache\n")
 print("Content-type: text/html\n\n")
 print("<html><head><title>Hello CGI World</title></head>\<body><h1 align=center>Hello HTML World</h1>\<hr/>\n")
@@ -10,5 +12,5 @@ print("Hello World<br/>\n")
 #  print("This program was generated at: {}\n<br/>".format(datetime.datetime.now()))
 #  print("Your current IP address is: {}<br/>".format(os.environ['REMOTE_ADDR']))
  
- # Print HTML footer
+# Print HTML footer
 print("</body></html>")
