9cd476e8b48d900f25e7e0128506cd9b7bbff1ee
Amrit-S <aksingh@ucsd.edu>
2021-04-22 22:01:30 -0700

    test

--- elks.codes/public_html/cgi-bin/python-hello-json-world.py
+++ elks.codes/public_html/cgi-bin/python-hello-json-world.py
@@ -3,8 +3,8 @@ import datetime
 import os
 
 #Print HTML header
-print("Cache-Control: no-cache")
-print("Content-type: application/json")
+print("Cache-Control: no-cache\r\n")
+print("Content-type: application/json\r\n\r\n")
 print('''
 {\n\t\"message\": \"Hello World\",\n
 \t\"date\": \"{}\",\n
