c9ce0a758d543989026e9f13701deaf10d0b417e
Amrit-S <aksingh@ucsd.edu>
2021-04-22 22:15:08 -0700

    test

--- elks.codes/public_html/cgi-bin/python-hello-json-world.py
+++ elks.codes/public_html/cgi-bin/python-hello-json-world.py
@@ -6,7 +6,7 @@ import os
 print("Cache-Control: no-cache")
 print("Content-type: application/json")
 print('''
-{"message": "Hello World\",\n
+{"message": "Hello World",\n
 "date": "{}",\n
 "currentIP": "{}"\n}\n
 '''.format(datetime.datetime.now(), os.environ['REMOTE_ADDR']))
