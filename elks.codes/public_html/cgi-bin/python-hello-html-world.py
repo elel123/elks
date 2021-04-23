2e69e826ef1f7b59f89dc8ec6af7eced7711955f
Amrit-S <aksingh@ucsd.edu>
2021-04-22 21:39:39 -0700

    test

--- elks.codes/public_html/cgi-bin/python-hello-html-world.py
+++ elks.codes/public_html/cgi-bin/python-hello-html-world.py
@@ -3,15 +3,26 @@ import datetime
 import os
 
 #Print HTML header
-print("Cache-Control: no-cache\n")
-print("Content-type: text/html\n\n")
-print("<html><head><title>Hello CGI World</title></head>\
+print('''
+Cache-Control: no-cache\n
+Content-type: text/html\n\n
+<html><head><title>Hello CGI World</title></head>\
 <body><h1 align=center>Hello HTML World</h1>\
-<hr/>")
+<hr/
+Hello World<br/>
+This program was generated at: a\n<br/>
+Your current IP address is: {}<br/>
+</body></html>
+''')
+# print("Cache-Control: no-cache\n")
+# print("Content-type: text/html\n\n")
+# print("<html><head><title>Hello CGI World</title></head>\
+# <body><h1 align=center>Hello HTML World</h1>\
+# <hr/>")
 
- print("Hello World<br/>")
- print("This program was generated at: {}\n<br/>".format('a'))
- print("Your current IP address is: {}<br/>".format('a'))
+#  print("Hello World<br/>")
+#  print("This program was generated at: {}\n<br/>".format('a'))
+#  print("Your current IP address is: {}<br/>".format('a'))
  
- # Print HTML footer
- print("</body></html>")
+#  # Print HTML footer
+#  print("</body></html>")
