56d634f6adccd8529e31b6843ef85ed977f52bed
Amrit-S <aksingh@ucsd.edu>
2021-04-22 21:45:52 -0700

    test

--- elks.codes/public_html/cgi-bin/python-hello-html-world.py
+++ elks.codes/public_html/cgi-bin/python-hello-html-world.py
@@ -3,9 +3,9 @@ import datetime
 import os
 
 #Print HTML header
+print("Cache-Control: no-cache")
+print("Content-type: text/html")
 print('''
-Cache-Control: no-cache\n
-Content-type: text/html\n\n
 <html><head><title>Hello CGI World</title></head>\
 <body><h1 align=center>Hello HTML World</h1>\
 <hr/>
