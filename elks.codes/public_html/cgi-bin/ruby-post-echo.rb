685171fad3d0d30777ba04bf79e43bcdf801696e
Elton Ho <ho.elton@gmail.com>
2021-04-24 02:07:19 -0700

    ruby post

--- elks.codes/public_html/cgi-bin/ruby-post-echo.rb
+++ elks.codes/public_html/cgi-bin/ruby-post-echo.rb
@@ -11,12 +11,5 @@ puts "<h1 align=center>POST Message Body</h1><hr/>\n"
 
 puts "<p><b>Message Body: </b>" + cgi.params + "</p>"
 
-# puts "<p>"
-# cgi.query_string.split("&").each { |var|
-#     puts var + "<br/>"
-# }
-
-# puts "</p>"
-
 puts "</body>"
 puts "</html>"
