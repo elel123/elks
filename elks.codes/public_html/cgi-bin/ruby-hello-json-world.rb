d2ec7e2cc8ccf7d947491416d0747e9c81187968
Elton Ho <ho.elton@gmail.com>
2021-04-23 23:48:05 -0700

    ruby json

---
+++ elks.codes/public_html/cgi-bin/ruby-hello-json-world.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+require 'json'
+
+time = Time.new
+
+json = '{"title":"Hello, Ruby!", \
+"heading":"Amrit, Elton, and Kelly were here - Hello Ruby", \
+"message":"' + time.inspect + '", \ 
+"IP":"' + ENV['REMOTE_ADDR'] + '"}'
+
+puts JSON.parse(json)
