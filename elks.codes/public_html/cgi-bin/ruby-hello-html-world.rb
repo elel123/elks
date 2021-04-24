7ed505166c3c7cc955807e1d092b8f6902bcf48e
Elton Ho <ho.elton@gmail.com>
2021-04-23 23:11:10 -0700

    added ruby to main page

--- elks.codes/public_html/cgi-bin/ruby-hello-html-world.rb
+++ elks.codes/public_html/cgi-bin/ruby-hello-html-world.rb
@@ -4,7 +4,7 @@ time = Time.new
 
 puts "Cache-Control: no-cache\n"
 puts "Content-type: text/html\n\n"
-puts "<html><head><title>Hello CGI World</title></head>"
+puts "<html><head><title>Hello Ruby CGI World</title></head>"
 puts "<body>"
 puts "<h1 align=center>Hello HTML World from Amrit, Elton, and Kelly</h1><hr/>\n"
 puts "<p>Hello World</p>"
