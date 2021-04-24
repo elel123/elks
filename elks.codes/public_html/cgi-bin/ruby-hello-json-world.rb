a12628346e6ec2309a543edfd2019fde000d2661
Elton Ho <ho.elton@gmail.com>
2021-04-23 23:59:29 -0700

    ruby json

--- elks.codes/public_html/cgi-bin/ruby-hello-json-world.rb
+++ elks.codes/public_html/cgi-bin/ruby-hello-json-world.rb
@@ -3,6 +3,9 @@ require 'json'
 
 time = Time.new
 
-json = '{"title":"Hello, Ruby!", "heading":"Amrit, Elton, and Kelly were here - Hello Ruby", "message":"' + time.inspect + '", "IP":"' + ENV['REMOTE_ADDR'] + '"}'
+json = '{"title":"Hello, Ruby!", \
+"heading":"Amrit, Elton, and Kelly were here - Hello Ruby", \
+"message":"' + time.inspect + '", \
+"IP":"' + ENV['REMOTE_ADDR'] + '"}'
 
 puts json
