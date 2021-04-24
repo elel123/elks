3d3e16adf8c9ac47870f18e8bf3d0c36d14adc5c
Elton Ho <ho.elton@gmail.com>
2021-04-24 01:42:20 -0700

    ruby get echo edit

--- elks.codes/public_html/cgi-bin/ruby-get-echo.rb
+++ elks.codes/public_html/cgi-bin/ruby-get-echo.rb
@@ -9,7 +9,9 @@ puts "<html><head><title>GET Echo</title></head>"
 puts "<body>"
 puts "<h1 align=center>GET Query String</h1><hr/>\n"
 
-puts "<p><b>Query String:</b>" + cgi.query_string + "</p>"
+puts "<p><b>Query String: </b>" + cgi.query_string + "</p>"
+
+puts cgi.query_string.split("&")
 
 puts "</body>"
 puts "</html>"
