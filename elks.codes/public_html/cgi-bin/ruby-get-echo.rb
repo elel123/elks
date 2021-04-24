433282ff7506681aa64dd9c0e12606f2dfc8c147
Elton Ho <ho.elton@gmail.com>
2021-04-24 01:34:01 -0700

    ruby get echo

---
+++ elks.codes/public_html/cgi-bin/ruby-get-echo.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+require 'cgi'
+
+cgi = CGI.new
+
+puts "Cache-Control: no-cache\n"
+puts "Content-type: text/html\n\n"
+puts "<html><head><title>GET Echo</title></head>"
+puts "<body>"
+puts "<h1 align=center>GET Query String</h1><hr/>\n"
+
+puts "<p><b>Query String:</b>" + cgi.query_string + "</p>"
+
+puts "</body>"
+puts "</html>"
