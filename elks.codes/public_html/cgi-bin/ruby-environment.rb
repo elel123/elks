da52315e31be07576e0ebaaf92b4e79eb540d570
Elton Ho <ho.elton@gmail.com>
2021-04-24 01:14:54 -0700

    displaying env vars ruby

--- elks.codes/public_html/cgi-bin/ruby-environment.rb
+++ elks.codes/public_html/cgi-bin/ruby-environment.rb
@@ -6,9 +6,12 @@ puts "<html><head><title>Environment Variables</title></head>"
 puts "<body>"
 puts "<h1 align=center>Environment Variables from Amrit, Elton, and Kelly</h1><hr/>\n"
 
+puts "<p>"
 ENV.keys.each { |env|
-    puts "<p><b>" + env + ":</b> " + ENV[env] + "</p>"
+    puts "<b>" + env + ":</b> " + ENV[env] + "<br/>"
 }
+
+puts "</p>"
 
 puts "</body>"
 puts "</html>"
