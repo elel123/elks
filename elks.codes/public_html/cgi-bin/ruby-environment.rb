aa3831ec61043babfc3140ad1c3a367794c052c4
Elton Ho <ho.elton@gmail.com>
2021-04-24 01:06:24 -0700

    made ruby env

---
+++ elks.codes/public_html/cgi-bin/ruby-environment.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+puts "Cache-Control: no-cache\n"
+puts "Content-type: text/html\n\n"
+puts "<html><head><title>Environment Variables</title></head>"
+puts "<body>"
+puts "<h1 align=center>Environment Variables from Amrit, Elton, and Kelly</h1><hr/>\n"
+
+ENV.keys.each { |env|
+    puts "<p>" + env + "</p>"
+}
+
+puts "</body>"
+puts "</html>"
--- elks.codes/public_html/cgi-bin/ruby-hello-json-world.rb
+++ elks.codes/public_html/cgi-bin/ruby-hello-json-world.rb
@@ -7,16 +7,10 @@ time = Time.new
 cgi = CGI.new
 
 
-
-json = '{"title":"Hello, Ruby!", "heading":"Amrit, Elton, and Kelly were here - Hello Ruby", "message":"' + time.inspect + '", "IP":"' + ENV["REMOTE_ADDR"] + '"}'
-json1 = '{"heading":"Amrit, Elton, and Kelly were here - Hello Ruby"}'
-json2 = '{"message":"'+ time.inspect+'"}'
-json3 = '{"IP":"' + 'ENV["REMOTE_ADDR"]' + '"}'
-
 puts cgi.header("type" => "application/json")
 puts ""
 puts ({
-    title: "Hello, Ruby!", 
+    title: "Hello, Ruby (JSON)!", 
     heading: "Amrit, Elton, and Kelly were here - Hello Ruby",
     message: time.inspect,
     IP: ENV["REMOTE_ADDR"]
