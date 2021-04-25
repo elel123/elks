34e8e09241f892be82860aef222a25596886eaa7
Amrit-S <aksingh@ucsd.edu>
2021-04-25 14:20:30 -0700

    test

--- elks.codes/public_html/cgi-bin/python-hello-json-world.py
+++ elks.codes/public_html/cgi-bin/python-hello-json-world.py
@@ -5,19 +5,13 @@ import os
 import json
 
 #Print HTML header
-# print("Cache-Control: no-cache") 
 cgitb.enable()
 print("Content-Type: application/json")
 print()
-# print("<TITLE>CGI script output</TITLE>")
-# print("<H1>This is my first CGI script</H1>")
-# print("Hello, world!")
 date = datetime.datetime.now()
-# print(os.environ["REMOTE_ADDR"])
-#print("Content-type: application/json")
 x = {
     "message": "Hello World",
     "date": str(date),
     "currentIP": str(os.environ["REMOTE_ADDR"])
 }
-print(json.dumps(x))
+print(json.dumps(x, indent=4))
