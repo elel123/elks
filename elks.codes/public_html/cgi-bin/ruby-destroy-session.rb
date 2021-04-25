e36ac444b735a394eee5a0067f8ce03878bdafe9
Elton Ho <ho.elton@gmail.com>
2021-04-25 15:32:22 -0700

    ruby session

---
+++ elks.codes/public_html/cgi-bin/ruby-destroy-session.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+require 'cgi'
+require 'cgi/session'
+
+cgi = CGI.new("html4")
+
+sess = CGI::Session.new(cgi)
+
+sess.delete
+
+print "<html>";
+print "<head>";
+print "<title>Perl Session Destroyed</title>";
+print "</head>";
+print "<body>";
+print "<h1>Session Destroyed</h1>";
+print "<a href=\"../hw2/ruby-cgiform.html\">Back to the Perl CGI Form</a><br />";
+print "<a href=\"/cgi-bin/ruby-sessions-1.rb\">Back to Page 1</a><br />";
+print "<a href=\"/cgi-bin/ruby-sessions-2.rb\">Back to Page 2</a>";
+print "</body>";
+print "</html>";
