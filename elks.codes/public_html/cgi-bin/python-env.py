f7f35996a9331c968cda475b55409785eec6d3b4
Amrit-S <aksingh@ucsd.edu>
2021-04-26 17:41:27 -0700

    python test

--- elks.codes/public_html/cgi-bin/python-env.py
+++ elks.codes/public_html/cgi-bin/python-env.py
@@ -8,5 +8,6 @@ print("Content-type: text/html")
 print('''
 <html><head><title>Environment Variables</title></head>\
 <body><h1 align=center>Environment Variables</h1>\
-<hr/> </body></html>
+<hr/>
 ''')
+print('''</body></html>''')
