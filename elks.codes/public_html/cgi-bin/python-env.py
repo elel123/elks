094c36d937d03d2fdd3fc1c5e4e9fe3f7f24e041
Amrit-S <aksingh@ucsd.edu>
2021-04-26 17:37:40 -0700

    python test

--- elks.codes/public_html/cgi-bin/python-env.py
+++ elks.codes/public_html/cgi-bin/python-env.py
@@ -9,4 +9,4 @@ print('''
 <html><head><title>Environment Variables</title></head>\
 <body><h1 align=center>Environment Variables</h1>\
 <hr/> </body></html>
-'''.format(datetime.datetime.now(), os.environ['REMOTE_ADDR']))
+''')
