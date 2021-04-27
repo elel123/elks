0170fe1d9bdf62ab82d63367fdba7fc8ac09f46a
Amrit-S <aksingh@ucsd.edu>
2021-04-26 17:10:52 -0700

    python test

--- elks.codes/public_html/cgi-bin/python-env.py
+++ elks.codes/public_html/cgi-bin/python-env.py
@@ -7,10 +7,6 @@ import json
 #Print HTML header
 print("Cache-Control: no-cache")
 print("Content-type: text/html")
-# print('''
-# <html><head><title>Environment Variables</title></head>\
-# <body><h1 align=center>Environment Variables</h1>\
-# <hr/> </body></html>''')
 print('''
 <html><head><title>Hello CGI World</title></head>\
 <body><h1 align=center>Hello HTML World</h1>\
@@ -20,5 +16,9 @@ This program was generated at: {}\n<br/>
 Your current IP address is: {}<br/>
 </body></html>
 '''.format(datetime.datetime.now(), os.environ['REMOTE_ADDR']))
+# print('''
+# <html><head><title>Environment Variables</title></head>\
+# <body><h1 align=center>Environment Variables</h1>\
+# <hr/> </body></html>''')
 # for key, value in os.environ.items():
 #     print("{}={}\n<br/>".format(key, value))
