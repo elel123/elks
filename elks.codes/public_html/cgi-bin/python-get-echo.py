f066092f2babfbdde0e5106d7bb895ce64b53c8e
Amrit-S <aksingh@ucsd.edu>
2021-04-26 18:21:27 -0700

    python test

--- elks.codes/public_html/cgi-bin/python-get-echo.py
+++ elks.codes/public_html/cgi-bin/python-get-echo.py
@@ -6,11 +6,10 @@ import os
 print("Cache-Control: no-cache")
 print("Content-type: text/html")
 print('''
-<html><head><title>Hello CGI World</title></head>\
-<body><h1 align=center>Hello HTML World</h1>\
+<html><head><title>Environment Variables</title></head>\
+<body><h1 align=center>Environment Variables</h1>\
 <hr/>
-Hello World<br/>
-This program was generated at: {}\n<br/>
-Your current IP address is: {}<br/>
-</body></html>
-'''.format(datetime.datetime.now(), os.environ['REMOTE_ADDR']))
+''')
+for key, value in os.environ.items():
+  print("{}={}\n<br/>".format(key, value))
+print('''</body></html>''')
