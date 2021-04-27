c776013cb9a0ca20cc16f6234e09a0bf1423fa68
Elton Ho <ho.elton@gmail.com>
2021-04-26 19:57:06 -0700

    python test elton

--- elks.codes/public_html/cgi-bin/python-get-echo.py
+++ elks.codes/public_html/cgi-bin/python-get-echo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from urllib.parse import urlparse, parse_qs
+# from urllib.parse import urlparse, parse_qs
 import os
 
 #URL='https://someurl.com/with/query_string?i=main&mode=front&sid=12ab&enc=+Hello'
@@ -14,13 +14,8 @@ Raw query string: {} <br/><br/>
 <table> Formatted Query String:
 '''.format(os.environ['QUERY_STRING']))
 
-<<<<<<< HEAD
 # parsed_url = urlparse(URL)
 # json = parse_qs(parsed_url.query)
-=======
-#parsed_url = urlparse(URL)
-json = parse_qs(URL)
->>>>>>> aeea7e0a5262ea79b6a6f033ca1a5f2027ca64fb
 
 # for item in json:
 #     print('''<tr><td>{}:</td><td>{}</td></tr>'''.format(item, json[item]))
