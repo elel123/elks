4007b7c9ebd77c160c39b5aedb0e08a7666e1729
Amrit-S <aksingh@ucsd.edu>
2021-04-26 18:32:35 -0700

    python test

--- elks.codes/public_html/cgi-bin/python-get-echo.py
+++ elks.codes/public_html/cgi-bin/python-get-echo.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 #Print HTML header
-print("Cache-Control: no-cache")
-print("Content-type: text/html")
-print('''
-<html><head><title>Environment Variables</title></head>\
-<body><h1 align=center>Environment Variables</h1>\
-<hr/>
-''')
-print('''</body></html>''')
+# print("Cache-Control: no-cache")
+# print("Content-type: text/html")
+# print('''
+# <html><head><title>Environment Variables</title></head>\
+# <body><h1 align=center>Environment Variables</h1>\
+# <hr/>
+# ''')
+# print('''</body></html>''')
+print("Hello")
