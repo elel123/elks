cf61340626724d474d08307af80e4fc0440ee271
Amrit-S <aksingh@ucsd.edu>
2021-04-26 19:34:41 -0700

    python test

--- elks.codes/public_html/cgi-bin/python-get-echo.py
+++ elks.codes/public_html/cgi-bin/python-get-echo.py
@@ -2,8 +2,8 @@
 from urllib.parse import urlparse, parse_qs
 import os
 
-URL='https://someurl.com/with/query_string?i=main&mode=front&sid=12ab&enc=+Hello'
-# URL = os.environ['QUERY_STRING']
+#URL='https://someurl.com/with/query_string?i=main&mode=front&sid=12ab&enc=+Hello'
+URL = os.environ['QUERY_STRING']
 
 print("Cache-Control: no-cache")
 print("Content-type: text/html\n")
