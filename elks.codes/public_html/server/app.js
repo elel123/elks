7e18e8541315c0397bce0fc6eb4bd9bf84ddb4c0
Amrit-S <aksingh@ucsd.edu>
2021-05-11 22:30:52 -0700

    added route for unique session cookies

--- elks.codes/public_html/server/app.js
+++ elks.codes/public_html/server/app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const http = require('http');
 const mongoose = require("mongoose");
+const uuid = require('uuid/v4');
 const config = require("./config");
 
 // MongoDB Connection via Mongoose
@@ -16,14 +17,13 @@ mongoose.set("useNewUrlParser", true);
 mongoose.connect(config.db.uri);
 mongoose.connection.once("open", async () => {
   console.log("Established connection to MongoDB.");
-  // console.log(config.db.uri);
-  // console.log(`Server starting at Port: ${config.app.port}`);
 });
 
 port = config.app.port;
 
 const app = express();
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json({limit:'5mb'}));
 app.use(express.urlencoded({extended:false}));
@@ -33,18 +33,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors({methods:["GET", "POST", "PUT", "DELETE"]}));
 
+// Routes
+app.use("/static", require("./routes/static"));
+app.use("/performance", require("./routes/performance"));
+
+app.get('/session', function(req, res) {
+
+  // session cookie already exists, return 
+  if (req.cookie.sessionId) {
+    return res.status(200).json(req.cookie.id);
+  }
+
+  // create unique session cookie
+  const id = uuid();
+  res.cookie('sessionId', id, { httpOnly: true });
+
+  return res.status(200).json(id);
+});
 
 app.get('/', function(req, res) {
   res.status(200).json({ message: "Welcome to the Node.js Express root"});
 });
-
-app.get('/test', function(req, res) {
-  res.status(200).json({ description: "Retreive every entry logged in the static table"});
-});
-
-app.use("/static", require("./routes/static"));
-app.use("/performance", require("./routes/performance"));
-
 
 //Create the server with the express app
 app.set('port', port);
