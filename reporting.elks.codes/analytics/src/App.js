0ae35da5420d3125626e443c0103946fe50b281c
Elton Ho <ho.elton@gmail.com>
2021-05-25 22:11:44 -0700

    editing gihub actions

--- reporting.elks.codes/analytics/src/App.js
+++ reporting.elks.codes/analytics/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React BABABOOEY
         </a>
       </header>
     </div>
