ccf2d08329511b6694af0ec49ab422bb40912db8
Kelly <kellyckc2017@gmail.com>
2021-06-02 20:23:28 -0700

    hw4 - add options

--- elks.codes/public_html/server/app.js
+++ elks.codes/public_html/server/app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors({methods:["GET", "POST", "PUT", "DELETE"]}));
+app.use(cors({methods:["GET", "POST", "PUT", "DELETE", "OPTIONS"]}));
 app.use(session({ secret: config.session.secret}))
 
 // Routes
