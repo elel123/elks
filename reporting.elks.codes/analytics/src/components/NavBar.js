011a5b976e0463a014f0b513a98fbd713a3db4ce
Amrit-S <aksingh@ucsd.edu>
2021-06-06 01:40:21 -0700

    updated navbar

--- reporting.elks.codes/analytics/src/components/NavBar.js
+++ reporting.elks.codes/analytics/src/components/NavBar.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect }  from "react";
+import React, {useEffect}  from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useHistory } from "react-router-dom";
 
 import { SITE_PAGES } from "../constants/links";
-import { setToken, getToken } from "../util/jwt";
-
-import './NavBar.css';
+import { setToken } from "../util/jwt";
+import ElksLogo from "../pages/images/android-chrome-192x192.png";
+import '../css/NavBar.css';
 
 export default function NavBar({ adminState, loginState }) {
     const history = useHistory();
@@ -28,35 +28,34 @@ export default function NavBar({ adminState, loginState }) {
         history.push(route);
     }
 
-
-    
-
     return (
         <Navbar className="bg-secondary" expand="lg">
-            <Navbar.Brand onClick={() => {history.push(SITE_PAGES.DASH);}} style={{"cursor" : "pointer", "color" : "#e6e6e6"}}>Elks.Code Reporting </Navbar.Brand>
+            <Navbar.Brand onClick={() => {history.push(SITE_PAGES.DASH);}}>
+                <img src={ElksLogo} className="nav-logo"/>
+                &nbsp;
+                Elks.Code Reporting 
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    {/* This is just for testing */}
-                    <Nav.Link onClick={() => {setLogIn(!logIn)}} style={{"color": "#e6e6e6"}}>Toggle</Nav.Link>
+                <Nav className="ml-auto">
 
                     {
                         logIn ?
                         (
                             <>
-                            <NavDropdown title="Visualizations" id="basic-nav-dropdown">
+                            {/* <NavDropdown title="Visualizations" id="basic-nav-dropdown">
                                 <Nav.Link onClick={() => {redirect(SITE_PAGES.DASH);}} style={{"color": "#000000"}}>Dashboard</Nav.Link>
                                 <Nav.Link onClick={() => {redirect(SITE_PAGES.REPORT);}} style={{"color": "#000000"}}>Activity Report</Nav.Link>
                                 <Nav.Link onClick={() => {redirect(SITE_PAGES.VIS3);}} style={{"color": "#000000"}}>Vis3</Nav.Link>
-                            </NavDropdown>
-                            <Nav.Link onClick={logoutHandler} style={{"color": "#e6e6e6"}}>Logout</Nav.Link>
+                            </NavDropdown> */}
+                            <Nav.Link onClick={() => {redirect(SITE_PAGES.DASH);}}>Dashboard</Nav.Link>
+                            <Nav.Link onClick={() => {redirect(SITE_PAGES.REPORT);}}>Activity Report</Nav.Link>
+                            { isAdmin ? (<Nav.Link onClick={() => {redirect(SITE_PAGES.ADMIN);}}>Admin</Nav.Link>) : null }
+                            <Nav.Link onClick={logoutHandler}>Logout</Nav.Link>
                             </>
                         ) :
                         null
                     } 
-
-                    { isAdmin ? (<Nav.Link onClick={() => {redirect(SITE_PAGES.ADMIN);}} style={{"color": "#e6e6e6"}}>Admin</Nav.Link>) : null }
-                    <Nav.Link onClick={() => {console.log(getToken())}} style={{"color": "#e6e6e6"}}>ViewToken</Nav.Link> 
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
